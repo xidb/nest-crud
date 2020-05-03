84b4764e4d1ea7edc90dbd94a35ef2fb553720dd
Dmitry Boytsov <boytsov.dm@gmail.com>
2020-05-03 17:50:57 +0300

    Delete user by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { IUser } from './interfaces/user.interface';
 import { UsersService } from './users.service';
@@ -7,13 +7,18 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Get()
+  async findAll(): Promise<IUser[]> {
+    return this.usersService.findAll();
+  }
+
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<void> {
     await this.usersService.create(createUserDto);
   }
 
-  @Get()
-  async findAll(): Promise<IUser[]> {
-    return this.usersService.findAll();
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<any> {
+    return this.usersService.remove(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,12 +8,16 @@ import { IUser } from './interfaces/user.interface';
 export class UsersService {
   constructor(@InjectModel('User') private readonly userModel: Model<IUser>) {}
 
+  async findAll(): Promise<IUser[]> {
+    return this.userModel.find().exec();
+  }
+
   async create(createUserDto: CreateUserDto): Promise<IUser> {
     const createdUser = new this.userModel(createUserDto);
     return createdUser.save();
   }
 
-  async findAll(): Promise<IUser[]> {
-    return this.userModel.find().exec();
+  async remove(id): Promise<any> {
+    return this.userModel.findByIdAndDelete(id);
   }
 }
